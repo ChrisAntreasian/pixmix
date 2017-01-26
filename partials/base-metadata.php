41a282f19ad9c7e4e5726083e975595336847e54
Chris Antreasian <chrisantreasian@gmail.com>
2017-01-26 00:10:04 -0500

    add metadata for responsive style contact page

--- partials/base-metadata.php
+++ partials/base-metadata.php
@@ -2,3 +2,4 @@
 <meta name="author" content="PixMix"/>
 <link rel="icon" type="image/ico" href="images/favicon.ico"/>
 <meta name="language" content="english" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
