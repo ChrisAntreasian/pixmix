ff045826f57c1ebf25fe9e1465100b028542df02
Chris Antreasian <chrisantreasian@gmail.com>
2017-02-12 17:44:16 -0500

    track mobile home partial

---
+++ partials/mobile-home.php
@@ -0,0 +1,61 @@
+
+<div class="m-home">
+    <img class="m-home__logo" src="../images/mobile/logo.png" alt="Pix Mix HD Video Services" />
+    <div class="m-home__sub-head"><h1>Video Services</h1></div>
+    <div class="m-home__phone"><a>617-254-0590</a></div>
+    <ul class="m-home__primary">
+        <li>
+            <h2>
+                MULTI &amp; SINGLE CAMERA
+            </h2>
+            <ul class="m-home__secondary">
+                <li>
+                    <a href="multi-camera">Multi-Camera</a>
+                </li>
+                <li>
+                    <a href="single-camera">Single Camera</a>
+                </li>
+                <li>
+                    <a href="hd">HD Video</a>
+                </li>
+            </ul>
+        </li>
+
+        <li>
+            <h2>
+                STUDIO FACILITIES &amp; RENTALS
+            </h2>
+            <ul class="m-home__secondary">
+                <li>
+                    <a href="boston">Boston HD Studio</a>
+                </li>
+                <li>
+                    <a href="metrowest">MetroWest Economy Studio</a>
+                </li>
+            </ul>
+        </li>
+
+        <li>
+            <h2>
+                LIVE STUDIO TRANSMISSIONS
+            </h2>
+            <ul class="m-home__secondary">
+                <li>
+                    <a href="live">Live Shots</a>
+                </li>
+                <li>
+                    <a href="webcasts">Webcasts</a>
+                </li>
+                <li>
+                    <a href="satellite-media">Satellite Media Tours</a>
+                </li>
+            </ul>
+        </li>
+        
+        <li>
+            <h2>
+                <a class="m-home__contact" href="contact">CONTACT</a>
+            </h2>
+        </li>
+    </ul>
+</div>
